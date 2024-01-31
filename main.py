683b12f07395083b0caef6f02b0ba86ae26a9188
ps <pio.szo@gmial.com>
2024-01-31 22:53:40 +0100

    Second part of exercise

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 from flask import Flask, render_template
 
+import tmdb_client
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    movies = [{}]
+    movies = [get_movie_info(movie) for movie in tmdb_client.get_movies()]
     return render_template("homepage.html", movies=movies)
+
+@app.route("/movie/<movie_id>")
+def movie_details(movie_id):
+    details = tmdb_client.get_single_movie(movie_id)
+    return render_template("movie_details.html", movie=details)
+
+@app.context_processor
+def utility_processor():
+    def tmdb_image_url(path, size):
+        return tmdb_client.get_poster_url(poster_path=path, size=size)
+    return {"tmdb_image_url": tmdb_image_url}
+
+def get_movie_info(movie):
+    movie_info = {
+        "id": movie["id"],
+        "title": movie["title"],
+        "poster": movie["poster_path"]
+    }
+    return movie_info
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ tmdb_client.py
@@ -0,0 +1,25 @@
+import requests
+
+def get_movies():
+    url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc"
+
+    headers = {
+        "accept": "application/json",
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZjE5ZDVhMWI4ODQ1NjMyNTZhNWIzYTY1MGMxZDI3YyIsInN1YiI6IjY1YmE1NmIxZTlkYTY5MDEyZGYyY2YyZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.7ScUMOxnQjwjLIv1EYgK0IEUzXHhEx4Owm5llTduM00"
+    }
+
+    response = requests.get(url, headers=headers)
+
+    return response.json()["results"]
+
+def get_single_movie(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
+    api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZjE5ZDVhMWI4ODQ1NjMyNTZhNWIzYTY1MGMxZDI3YyIsInN1YiI6IjY1YmE1NmIxZTlkYTY5MDEyZGYyY2YyZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.7ScUMOxnQjwjLIv1EYgK0IEUzXHhEx4Owm5llTduM00"
+    headers = {
+        "Authorization": f"Bearer {api_token}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
+def get_poster_url(poster_path, size="w342"):
+    return f"https://image.tmdb.org/t/p/{size}/{poster_path}"
